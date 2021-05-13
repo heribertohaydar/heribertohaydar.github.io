1968a127f1596f3928545bb80782eb815a441f61
Heriberto Haydar <heriberto.haydar@gmail.com>
2021-05-13 15:57:52 -0500

    add state messages, modify css

--- mdk/mdk.js
+++ mdk/mdk.js
@@ -6,10 +6,10 @@
 
 var player;
 var options = {
-    "url" : "",
-    "logging" : false,
-    "realtime" :false,
-    "jwt" : false
+    "url": "",
+    "logging": false,
+    "realtime": false,
+    "jwt": false
 }
 var db = new restdb("6091adf3f2fc22523a42c81f", options);
 var query = {}; // get all records
@@ -23,10 +23,16 @@ function getRandomInt(max) {
 }
 
 
-async function loadLatestAddedSongs() {
-    var hints = {"$max": 5, "$orderby": {"_id": -1}};
-    db.playlist.find({}, hints, function(err, res){
-        if (!err){
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function loadLatestAddedSongs() {
+    var hints = { "$max": 7, "$orderby": { "_id": -1 } };
+    db.playlist.find({}, hints, function (err, res) {
+        if (!err) {
             for (const r of res) {
                 let element = document.getElementById('latestAddedSongSection');
                 let div = document.createElement('div');
@@ -34,13 +40,13 @@ async function loadLatestAddedSongs() {
                 let pPosted = document.createElement('p');
                 let i = document.createElement('i');
                 let br = document.createElement('br');
-                let nodeTitle = document.createTextNode(r.dateAdded.substring(0,10));
+                let nodeTitle = document.createTextNode(r.dateAdded.substring(0, 10));
                 let nodePosted = document.createTextNode("Added by: ");
                 let iText = document.createTextNode(r.uploader);
                 let aText = document.createTextNode("Play");
 
                 let ahref = document.createElement('a');
-                ahref.href="javascript:playLatestAddedSong('" + r.idYoutubeVideo + "');"
+                ahref.href = "javascript:playLatestAddedSong('" + r.idYoutubeVideo + "');"
                 ahref.appendChild(aText);
 
                 i.appendChild(iText);
@@ -63,29 +69,31 @@ async function loadLatestAddedSongs() {
                 element.appendChild(div);
 
             }
-        }});
+        }
+    });
 }
 
 function onYouTubeIframeAPIReady() {
-    db.playlist.find(query, hints, function(err, res){
-    if (!err){
-        res.sort(() => Math.random() - 0.5);
-        listVideos = JSON.stringify(res.map(e => e.idYoutubeVideo)).replace("[","").replace("]","").replace(/\"/g, "");
-        initialVideo = res[getRandomInt(res.length)]['idYoutubeVideo'];
-        player = new YT.Player('video-placeholder', {
-        width: 600,
-        height: 400,
-        videoId: initialVideo,
-        playerVars: {
-            controls: 1,
-            showinfo: 1,
-            playlist: listVideos,
-            enablejsapi: 1,
-            loop: 1,
-            modestbranding: 1
-        },
-        });
-    }});
+    db.playlist.find(query, hints, function (err, res) {
+        if (!err) {
+            res.sort(() => Math.random() - 0.5);
+            listVideos = JSON.stringify(res.map(e => e.idYoutubeVideo)).replace("[", "").replace("]", "").replace(/\"/g, "");
+            initialVideo = res[getRandomInt(res.length)]['idYoutubeVideo'];
+            player = new YT.Player('video-placeholder', {
+                width: 600,
+                height: 400,
+                videoId: initialVideo,
+                playerVars: {
+                    controls: 1,
+                    showinfo: 1,
+                    playlist: listVideos,
+                    enablejsapi: 1,
+                    loop: 1,
+                    modestbranding: 1
+                },
+            });
+        }
+    });
 }
 
 function getNewReleasesFromSpotify() {
@@ -97,12 +105,11 @@ function getNewReleasesFromSpotify() {
     var data = "grant_type=client_credentials";
     xhr.send(data);
     var tokenSpotify;
-    xhr.onload=function(){
-        tokenSpotify="Bearer " + JSON.parse(xhr.responseText).access_token;
-        console.log("Token Spotify: " + tokenSpotify);
+    xhr.onload = function () {
+        tokenSpotify = "Bearer " + JSON.parse(xhr.responseText).access_token;
         getAuthorizedListSpotify(tokenSpotify);
-        };
-    
+    };
+
 }
 
 
@@ -114,12 +121,12 @@ function getAuthorizedListSpotify(token) {
     req.setRequestHeader("Content-Type", "application/json");
     req.setRequestHeader("Authorization", token);
     req.send();
-    req.onload=function(){
-        dataSpotify=JSON.parse(req.responseText);
+    req.onload = function () {
+        dataSpotify = JSON.parse(req.responseText);
         for (const album of dataSpotify.albums.items) {
-            buildSopotifyElements(album.name, 
-                album.artists[0].name, 
-                album.release_date, 
+            buildSopotifyElements(album.name,
+                album.artists[0].name,
+                album.release_date,
                 album.artists[0].external_urls.spotify)
         }
     };
@@ -138,25 +145,25 @@ function buildSopotifyElements(song, artist, date, link) {
     let divDateText = document.createTextNode("Release on: " + date);
     let br = document.createElement('br');
 
-    a.href=link;
-    a.target="_blank";
-    a.style="color: pink"
+    a.href = link;
+    a.target = "_blank";
+    a.style = "color: pink"
     a.appendChild(aText);
-    
+
     divSong.classList.add('ads');
     divSong.appendChild(divSongText);
-    
+
     divArtist.classList.add('repoTitle');
     divArtist.appendChild(divArtistText);
-    
+
     divDate.classList.add('issueDetail');
     divDate.appendChild(divDateText);
-    
+
     divSong.appendChild(divArtist);
     divSong.appendChild(divDate);
     divDate.appendChild(br);
     divDate.appendChild(a);
-    
+
     element.appendChild(divSong);
 
 }
@@ -168,112 +175,120 @@ function pad2(n) {
 function validVideoId(id) {
     var img = new Image();
     img.src = "http://img.youtube.com/vi/" + id + "/0.jpg";
-    id=id;// I don't get it how this works!!!! but when copy id value .. inner function works
+    id = id;// I don't get it how this works!!!! but when copy id value .. inner function works
     img.onload = function () {
         if (this.width < 320) {
-            console.log("The following video doesn't exist on Youtube database. " + id);
+            sendMessage("The song pasted doesn't exist on youtube database, please verify your url.");
         } else {
-            console.log("The following video exists on Youtube database. " + id);
             var uploader_text = $('#uploader').val();
-            if (uploader_text == "") uploader_text="Not defined."
+            if (uploader_text == "") uploader_text = "mdk."
             var date = new Date();
-            var month = pad2(date.getMonth()+1);//months (0-11)
+            var month = pad2(date.getMonth() + 1);//months (0-11)
             var day = pad2(date.getDate());//day (1-31)
-            var year= date.getFullYear();
-            var formattedDate =  day+"/"+month+"/"+year;
-            var jsondata = {"idYoutubeVideo": id, "uploader": uploader_text, "dateAdded": formattedDate};
+            var year = date.getFullYear();
+            var formattedDate = day + "/" + month + "/" + year;
+            var jsondata = { "idYoutubeVideo": id, "uploader": uploader_text, "dateAdded": formattedDate };
             var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://mdkv10-85fe.restdb.io/rest/playlist",
-            "method": "POST",
-            "headers": {
-                "content-type": "application/json",
-                "x-apikey": "6091adf3f2fc22523a42c81f",
-                "cache-control": "no-cache"
-            },
-            "processData": false,
-            "data": JSON.stringify(jsondata)
+                "async": true,
+                "crossDomain": true,
+                "url": "https://mdkv10-85fe.restdb.io/rest/playlist",
+                "method": "POST",
+                "headers": {
+                    "content-type": "application/json",
+                    "x-apikey": "6091adf3f2fc22523a42c81f",
+                    "cache-control": "no-cache"
+                },
+                "processData": false,
+                "data": JSON.stringify(jsondata)
             }
 
             $.ajax(settings).done(function (response) {
-                setTimeout(() => {  
-                    reloadNewSong(id); 
-                    console.log("The following video was added to MDK database. " + id);
-                }, 1000);
-                
-            });                    
+                setTimeout(() => {
+                    reloadNewSong(id);
+                }, 500);
+            });
         }
     }
 }
 
 function reloadNewSong(id) {
     //player.cueVideoById(id);
-     //player.cuePlaylist()
+    //player.cuePlaylist()
     //player.cueVideoById(video_id);
-    id=id;
-    db.playlist.find({}, {}, function(err, res){
-    if (!err){
-        t=res[0];
-        f=res[res.length-1];
-        //res[0]=res[res.length-1];
-        //res[res.length-1]=t;
-        res.sort(() => Math.random() - 0.5);
-        res[0]=f;
-        res[res.length-1]=t;
-        listVideos = JSON.stringify(res.map(e => e.idYoutubeVideo)).replace("[","").replace("]","").replace(/\"/g, "");
-        player.loadVideoById(id);
-        player.cuePlaylist(listVideos);
-        console.log("The following video was added to play list. " + id);
-    }});
-
-}
-
-function reloadPlayList()  {
-    db.playlist.find({}, {}, function(err, res){
-        if (!err){
+    id = id;
+    db.playlist.find({}, {}, function (err, res) {
+        if (!err) {
+            t = res[0];
+            f = res[res.length - 1];
+            //res[0]=res[res.length-1];
+            //res[res.length-1]=t;
             res.sort(() => Math.random() - 0.5);
-            listVideos = JSON.stringify(res.map(e => e.idYoutubeVideo)).replace("[","").replace("]","").replace(/\"/g, "");
+            res[0] = f;
+            res[res.length - 1] = t;
+            listVideos = JSON.stringify(res.map(e => e.idYoutubeVideo)).replace("[", "").replace("]", "").replace(/\"/g, "");
+            player.loadVideoById(id);
             player.cuePlaylist(listVideos);
-            console.log("Playlist was reload succesfully.");
-        }});
+            sendMessage("Your song was added successfuly to MDK playlist.");
+            let element = document.getElementById('latestAddedSongSection');
+            removeAllChildNodes(element);
+            loadLatestAddedSongs();
+
+        }
+    });
+
 }
 
+function reloadPlayList() {
+    db.playlist.find({}, {}, function (err, res) {
+        if (!err) {
+            res.sort(() => Math.random() - 0.5);
+            listVideos = JSON.stringify(res.map(e => e.idYoutubeVideo)).replace("[", "").replace("]", "").replace(/\"/g, "");
+            player.cuePlaylist(listVideos);
+            sendMessage("Playlist reloaded successfuly.");
+        }
+    });
+}
+
+function sendMessage(m) {
+    document.getElementById("MKD.message").innerHTML = m;
+    document.getElementById("MKD.message").style.display = "block";
+
+}
 function playLatestAddedSong(id) {
     //player.cueVideoById(id);
-     //player.cuePlaylist()
+    //player.cuePlaylist()
     //player.cueVideoById(video_id);
-    id=id;
+    id = id;
     player.loadVideoById(id);
     //player.cuePlaylist(listVideos);
-    console.log("Play this video. " + id);
+    //console.log("Play this video. " + id);
 }
 
 
 function addNewSong() {
-
+    sendMessage("Adding new song to MDK playlist, please wait.");
     var url = $('#urlSong').val();
     $('#urlSong').val("");
     var video_id = url.split('v=')[1];
-    if (video_id==null) {
+    if (video_id == null) {
         video_id = url.split('.be/')[1];;
     }
-    if (video_id==null) {
+    if (video_id == null) {
         return;
     }
     var ampersandPosition = video_id.indexOf('&');
-    if(ampersandPosition != -1) {
+    if (ampersandPosition != -1) {
         video_id = video_id.substring(0, ampersandPosition);
     }
-    query={"idYoutubeVideo":video_id};
-    hints={};
-    db.playlist.find(query, hints, function(err, res){
-        if (!err){
+    query = { "idYoutubeVideo": video_id };
+    hints = {};
+    db.playlist.find(query, hints, function (err, res) {
+        if (!err) {
             if (res.length > 0) {
-                console.log("The following video already exists on MDK database. " + video_id);
+                sendMessage("Your song already exists on MDK playlist, please add one different.");
             } else {
-                console.log("The following video doesn't exist on MDK database. " + video_id);
                 validVideoId(video_id);
             }
-        }});
+        }
+    });
 }
